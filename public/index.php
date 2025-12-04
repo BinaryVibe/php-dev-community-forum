5c8db476cc81e223f0a2a59e4a8c953859d32c3f
NextStep-IM <malikharis2003@gmail.com>
2025-12-05 01:04:54 +0500

    Use HTML instead of passing string to echo

--- public/index.php
+++ public/index.php
@@ -46,39 +46,39 @@ session_start();
 
                 </div>
 
-                <?php
-                if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true) {
-                    
-                    echo "
-                        <div class='d-flex flex-nowrap column-gap-2'>
-                            <a href='new_post.php' class='nav-item btn btn-primary' role='button'>
-                                <i class='bi bi-plus-lg'></i>
-                            </a>
-                            
-
-                            <div class='dropdown'>
-                                <button class='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                                    <i class='bi bi-person-circle'></i>
-                                </button>
-                                <ul class='dropdown-menu dropdown-menu-end'>
-                                    <li><a class='dropdown-item' href='#'>Action</a></li>
-                                    <li><a class='dropdown-item' href='#'>Another action</a></li>
-                                    <li><a class='dropdown-item' href='#'>Something else here</a></li>
-                                </ul>
-                            </div>
+                <?php if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true): ?>
 
 
+                    <div class='d-flex flex-nowrap column-gap-2'>
+                        <a href='new_post.php' class='nav-item btn btn-primary' role='button'>
+                            <i class='bi bi-plus-lg'></i>
+                        </a>
+
+
+                        <div class='dropdown'>
+                            <button class='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown'
+                                aria-expanded='false'>
+                                <i class='bi bi-person-circle'></i>
+                            </button>
+                            <ul class='dropdown-menu dropdown-menu-end'>
+                                <li><a class='dropdown-item' href='#'>Action</a></li>
+                                <li><a class='dropdown-item' href='#'>Another action</a></li>
+                                <li><a class='dropdown-item' href='#'>Something else here</a></li>
+                            </ul>
                         </div>
-                    ";
-                } else {
-                    echo "
+
+
+                    </div>
+
+                <?php else: ?>
+
                     <div class='auth-btns d-flex flex-nowrap column-gap-2'>
                         <a href='login.html' class='nav-item btn btn-primary' role='button'>Login</a>
                         <a href='signup.html' class='nav-item btn btn-outline-primary' role='button'>Sign Up</a>
                     </div>
-                    ";
-                }
-                ?>
+
+
+                <?php endif; ?>
             </div>
         </nav>
     </header>
